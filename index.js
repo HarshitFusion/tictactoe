f244a0302c63b8d3bedeb54253e9fe5bc54e9fae
harshitfusion <mauryaharshit016@gmail.com>
2025-01-31 17:19:10 +0530

    feat adding the winner function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let button = document.querySelectorAll(".box");
 let reset = document.querySelector("#reset-button");
-
+let turn0 = true;
 
 const winpatterns =[
     [0,1,2],
@@ -16,5 +16,30 @@ const winpatterns =[
 button.forEach((box) =>{
       box.addEventListener("click",() => {
         console.log("box was clicked");
-      })
-})
+        if(turn0){
+            box.innerText = "O";
+            turn0 = false;
+        }
+        else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        winner();
+      });
+});
+
+const winner = () => {
+    for (let pattern of winpatterns) {
+           let pos1val = button[pattern[0]].innerText;
+           let pos2val =  button[pattern[1]].innerText;
+           let pos3val = button[pattern[2]].innerText;
+           
+           if(pos1val != "" && pos2val != "" && pos3val != ""){
+            if(pos1val === pos2val && pos2val === pos3val){
+                console.log("winner");
+            }
+           }
+    }
+};
